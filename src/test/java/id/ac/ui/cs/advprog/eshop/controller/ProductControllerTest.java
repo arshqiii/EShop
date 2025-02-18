fbcabd084d829bc36efb66f6ce2027a31a62fa78
arshqiii <marshq23@gmail.com>
2025-02-18 11:32:43 +0700

    feat : added new unit tests

--- src/test/java/id/ac/ui/cs/advprog/eshop/controller/ProductControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/ProductControllerTest.java
@@ -41,6 +41,20 @@ public class ProductControllerTest {
     }
 
     @Test
+    void testCreateProduct() throws Exception {
+        Product product = new Product();
+        product.setProductName("Test");
+        product.setProductQuantity(10);
+
+        mockMvc.perform(post("/product/create")
+                        .flashAttr("product", product))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("list"));
+
+        verify(productService, times(1)).create(any(Product.class));
+    }
+
+    @Test
     void testUpdateProductPage() throws Exception {
         Product product = new Product();
         product.setProductID(UUID.randomUUID().toString());
@@ -49,5 +63,41 @@ public class ProductControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("editProduct"))
                 .andExpect(model().attributeExists("product"));
+    }
+
+    @Test
+    void testUpdateProduct() throws Exception {
+        Product product = new Product();
+        product.setProductID(UUID.randomUUID().toString());
+        product.setProductName("Test");
+        product.setProductQuantity(10);
+
+        when(productService.findById(product.getProductID())).thenReturn(product);
+
+        mockMvc.perform(post("/product/edit/" + product.getProductID())
+                .flashAttr("product", product))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("list"));
+
+        verify(productService, times(1)).update(any(Product.class));
+    }
+
+    @Test
+    void testDeleteProduct() throws Exception {
+        String productID = UUID.randomUUID().toString();
+        when(productService.findById(productID)).thenReturn(null);
+
+        mockMvc.perform(delete("/product/delete/" + productID))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/product/list"));
+        verify(productService, times(1)).delete(productID);
+    }
+
+    @Test
+    void testListProductPage() throws Exception {
+        mockMvc.perform(get("/product/list"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("productList"))
+                .andExpect(model().attributeExists("products"));
     }
 }
